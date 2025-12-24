f0735462e7138b6413f17e57e554aa1ee0fb5ed4
confushon <aw240057@student.uthm.edu.my>
2025-12-24 13:04:42 +0800

    Update passenger_queue.py

--- queues/passenger_queue.py
+++ queues/passenger_queue.py
@@ -24,3 +24,39 @@ class PassengerQueue:
       self.rear = new_node
 
     self.size += 1
+
+  def dequeue(self, current_time):
+    """Remove passenger from queue and calculate waiting time"""
+    if self.is_empty():
+      return None, 0
+
+    removed_passenger = self.front
+    self.front = self.front.next
+
+    if self.front is None:
+      self.rear = None
+
+    self.size -= 1
+
+    waiting_time = current_time - removed_passenger.arrival_time
+    return removed_passenger.passenger_id, waiting_time
+
+  def get_queue_size(self):
+    """Display passengers currently in queue"""
+    passenger = []
+    current = self.front
+
+    while current:
+      passenger.append(
+        f"{current.passenger_id} (arrived at {current.arrival_time})"
+      )
+      current = current.next
+
+    return passengers
+
+#Testing
+queue = PassengerQueue()
+queue.enqueue("S001", 5)
+queue.enqueue("S002", 7)
+pid, wait = queue.dequeue(10)
+print(pid, wait)
