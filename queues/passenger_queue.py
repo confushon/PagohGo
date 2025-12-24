938f676f394ff2dd7bc19737de4f9e9c10afd99c
confushon <aw240057@student.uthm.edu.my>
2025-12-24 12:38:27 +0800

    Update passenger_queue.py

--- queues/passenger_queue.py
+++ queues/passenger_queue.py
@@ -3,3 +3,12 @@ class PassengerNode:
     self.passenger_id = passenger_id
     self.arrival_time = arrival_time
     self.next = None
+
+class PassengerQueue:
+  def __init__(self):
+    self.front = None
+    self.rear = None
+    self.size = 0
+
+  def is_empty(self):
+    return self.front is None
